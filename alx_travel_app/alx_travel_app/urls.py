5f2311c11e6f77ae47a8d9f17ba106de17f5d794
Alpha Infinos <kevinmullerhi@gmail.com>
2025-06-15 16:43:54 +0300

    views and urls

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -20,3 +20,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),  # This exposes /api/listings/ and /api/bookings/
+]
